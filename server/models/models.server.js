a78bd945ef0e85e9ce290c78725c570f75bbf556
sheila mcdaniel <smcdaniel617@aol.com>
2018-12-05 15:28:34 -0500

    data service

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -6,7 +6,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) {
 // check if running remotely
 
 var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-var password = "process.env.MLAB_PASSWORD_WEBDEV";
+var password = process.env.MLAB_PASSWORD_WEBDEV;
 connectionString = "mongodb://" + username + ":" + password;
 connectionString += "@ds023463.mlab.com:23463/heroku_gt7dvhqk"; // use yours
 }
